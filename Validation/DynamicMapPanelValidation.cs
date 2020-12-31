c5657e3dd1d27734f73a942a39241a109dea2f39
nikkiwritescode <nicole.farris@outlook.com>
2020-12-31 07:39:03 -0600

    upgraded to .net 4.5

---
+++ Validation/DynamicMapPanelValidation.cs
@@ -0,0 +1,45 @@
+﻿using CustomStreetManager.Panels.MainWindow;
+
+namespace CustomStreetManager.Validation
+{
+    public class DynamicMapPanelValidation
+    {
+        private MainWindow _mainWindow;
+
+        public DynamicMapPanelValidation(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        public bool TwoDynamicDropdownsAreNotNullAndNotEqualToEachOther()
+        {
+            return _mainWindow.map1OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map2OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map1OrderDropdown.SelectedItem.ToString() != _mainWindow.map2OrderDropdown.SelectedItem.ToString();
+        }
+
+        public bool ThreeDynamicDropdownsAreNotNullAndNotEqualToEachOther()
+        {
+            return _mainWindow.map1OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map2OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map3OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map1OrderDropdown.SelectedItem.ToString() != _mainWindow.map2OrderDropdown.SelectedItem.ToString() && 
+                   _mainWindow.map1OrderDropdown.SelectedItem.ToString() != _mainWindow.map3OrderDropdown.SelectedItem.ToString() && 
+                   _mainWindow.map2OrderDropdown.SelectedItem.ToString() != _mainWindow.map3OrderDropdown.SelectedItem.ToString();
+        }
+
+        public bool FourDynamicDropdownsAreNotNullAndNotEqualToEachOther()
+        {
+            return _mainWindow.map1OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map2OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map3OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map4OrderDropdown.SelectedItem != null && 
+                   _mainWindow.map1OrderDropdown.SelectedItem.ToString() != _mainWindow.map2OrderDropdown.SelectedItem.ToString() && 
+                   _mainWindow.map1OrderDropdown.SelectedItem.ToString() != _mainWindow.map3OrderDropdown.SelectedItem.ToString() &&
+                   _mainWindow.map1OrderDropdown.SelectedItem.ToString() != _mainWindow.map4OrderDropdown.SelectedItem.ToString() && 
+                   _mainWindow.map2OrderDropdown.SelectedItem.ToString() != _mainWindow.map3OrderDropdown.SelectedItem.ToString() && 
+                   _mainWindow.map2OrderDropdown.SelectedItem.ToString() != _mainWindow.map4OrderDropdown.SelectedItem.ToString() && 
+                   _mainWindow.map3OrderDropdown.SelectedItem.ToString() != _mainWindow.map4OrderDropdown.SelectedItem.ToString();
+        }
+    }
+}
--- bin/Release/_windowspatchscript_extractDisc.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-wit extract "%~1" ..\_working_directory/ -v --enc decrypt --quiet
-exit
